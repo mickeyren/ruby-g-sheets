af6ebe5cbcc099b2e0397033e41266c5fa3c684b
David Ang <davidang09@gmail.com>
2019-10-21 04:20:33 +0800

    start receiving parameters from the http request

--- app.rb
+++ app.rb
@@ -15,7 +15,12 @@ namespace '/api' do
     DB = Sequel.connect(ENV['DATABASE_URL'] || 'postgres://davidang:@localhost:5432/ruby-g-sheets') 
     dataset = DB[:users]
     
-    dataset.insert(first_name: 'Sharon', last_name: 'Cuneta', email: 'sharon@gmail.com', phone: '12345')
+    dataset.insert(
+      first_name: params[:first_name], 
+      last_name: params[:last_name], 
+      email: params[:email], 
+      phone: params[:phone],
+      timestamp: params[:timestamp])
 
     halt(200, { message: 'Hello World' }.to_json)
   end
