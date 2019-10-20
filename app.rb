538e282fb1bbbff25496dec6084026bfe402bda3
David Ang <davidang09@gmail.com>
2019-10-21 05:03:05 +0800

    initial attempt on sending to talkpush

--- app.rb
+++ app.rb
@@ -2,6 +2,11 @@ require 'sinatra'
 require 'sinatra/namespace'
 require 'sequel'
 
+require 'net/http'
+require 'uri'
+require 'json'
+
+
 class Application < Sinatra::Base
 
 end
@@ -16,12 +21,35 @@ namespace '/api' do
     dataset = DB[:users]
     
     dataset.insert(
-      first_name: params[:first_name], 
-      last_name: params[:last_name], 
-      email: params[:email], 
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
       phone: params[:phone],
       timestamp: params[:timestamp])
 
-    halt(200, { message: 'Hello World' }.to_json)
+    # talkpush
+    uri = URI.parse('https://my.talkpush.com/api/talkpush_services/campaigns/1/campaign_invitations')
+
+    header = { 'Cache-Control': 'no-cache', 'Content-Type': 'application/json' }
+    params = { 
+      api_key: '4ey: 448530ba23eef6b45ffbc95d',
+      api_secret: 'eet: ee2f724ba060f82ddf58923a',
+      campaign_invitation: {
+        first_name: params[:first_name],
+        last_name: params[:last_name],
+        email: params[:email],
+        user_phone_number: params[:phone]
+      }
+    }
+
+    # Create the HTTP objects
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Post.new(uri.request_uri, header)
+    request.body = params.to_json
+
+    # Send the request
+    response = http.request(request)
+
+    halt(200, response.to_json)
   end
 end
